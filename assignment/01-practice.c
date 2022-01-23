88cec32786e4d2ee6b10f631b74439a67022f29a
Priyanshu <2003priyanshusingh@gmail.com>
2022-01-23 10:24:40 +0530

    new changes

--- assignment/01-practice.c
+++ assignment/01-practice.c
@@ -2,10 +2,10 @@
 
 int main() {
     int length , breadth ;
-    printf("what is length of rectangle\n");
+    printf("length of rectangle\n");
     scanf("%d", &length);
 
-    printf("what is the breadth of the rctangle\n");
+    printf("breadth of the rctangle\n");
     scanf("%d",&breadth);
 
     printf("area of the your reactangle is %d",length*breadth);
